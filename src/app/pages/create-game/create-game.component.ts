5fe021af545f11cb8ed8e0dfbd3273690c4451f0
Daniel Aranda Martin <danielarandamar88@gmail.com>
2021-09-23 15:19:56 -0500

    Screen for create new category

--- src/app/pages/create-game/create-game.component.ts
+++ src/app/pages/create-game/create-game.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-game',
@@ -7,9 +8,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateGameComponent implements OnInit {
 
-  constructor() { }
+  public formCustom: FormGroup;
+
+  constructor(
+    private fb: FormBuilder
+  ) { 
+
+    this.formCustom = new FormGroup({});
+    this.createForm();
+    this.loadDataForm();
+
+  }
+
+  /**
+   * Get all questions from the form
+   */
+  get questionsForm() {
+    return this.formCustom.get('questions') as FormArray;
+  }
+
+
+
+
 
   ngOnInit(): void {
+    console.log(this.questionsForm);
   }
+
+  /**
+    * Creates the form to create new game
+    */
+  createForm() {
+    this.formCustom = this.fb.group({
+      categoryName: ['',  [Validators.required]],
+      categoryDescription:    ['',  [Validators.required]],
+      password: ['',  [Validators.required]],
+      repeatPassword: ['', [Validators.required]],
+      questions: this.fb.array([]),
+    });
+  }
+
+  /**
+   * Load initial data with questions
+   */
+  loadDataForm() {
+    ['question1','question2','question3'].forEach(value => this.questionsForm.push( this.fb.control(value) ));
+  }
+
+  /**
+   * Add question to the form
+   */
+  addQuestion() {
+    this.questionsForm.push(this.fb.control('',Validators.required));
+  }
+
+  /**
+   * Remove question from the form
+   * @param i 
+   */
+  removeQuestion(i: number) {
+    this.questionsForm.removeAt(i);
+  }
+
+
+
+
 
 }
